a512910363076c7ec99f73d48ab8687e3b38cf58
Wes Mangum <wesmangum24@gmail.com>
2014-04-30 09:58:09 -0500

    Added weather info and clouds

--- app/js/es6/main.es6.js
+++ app/js/es6/main.es6.js
@@ -19,17 +19,26 @@
     let styles =[{'featureType':'water','elementType':'geometry','stylers':[{'color':'#a2daf2'}]},{'featureType':'landscape.man_made','elementType':'geometry','stylers':[{'color':'#f7f1df'}]},{'featureType':'landscape.natural','elementType':'geometry','stylers':[{'color':'#d0e3b4'}]},{'featureType':'landscape.natural.terrain','elementType':'geometry','stylers':[{'visibility':'off'}]},{'featureType':'poi.park','elementType':'geometry','stylers':[{'color':'#bde6ab'}]},{'featureType':'poi','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'poi.medical','elementType':'geometry','stylers':[{'color':'#fbd3da'}]},{'featureType':'poi.business','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'geometry.stroke','stylers':[{'visibility':'off'}]},{'featureType':'road','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#ffe15f'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#efd151'}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#ffffff'}]},{'featureType':'road.local','elementType':'geometry.fill','stylers':[{'color':'black'}]},{'featureType':'transit.station.airport','elementType':'geometry.fill','stylers':[{'color':'#cfb2db'}]}];
     let mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.MAP, styles: styles};
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    debugger;
+
+    let weatherLayer = new google.maps.weather.WeatherLayer({
+      temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+    });
+    weatherLayer.setMap(map);
+
+    let cloudLayer = new google.maps.weather.CloudLayer();
+    cloudLayer.setMap(map);
   }
 
   function add() {
     let zip = $('#zip').val();
     $('#zip').val('');
     $('#zip').focus();
-    weather(zip);
+    getWeather(zip);
     show(zip);
   }
 
-  function weather(zip) {
+  function getWeather(zip) {
     let url = 'http://api.wunderground.com/api/11cb146a56f12140/forecast10day/q/'+zip+'.json?callback=?';
     $.getJSON(url, data=>{
       $('#charts').append(`<div class=chart data-zip=${zip}></div>`);
@@ -40,17 +49,7 @@
 
   function tenDay(data, zip) {
     let forecast = data.forecast.simpleforecast.forecastday;
-    // let graph = [];
-    //
-    // for (var i = 0; i < forecast.length; i++) {
-    //   let day = {};
-    //   day.high = forecast[i].high.fahrenheit;
-    //   day.low = forecast[i].low.fahrenheit;
-    //   day.date = `${forecast[i].date.month}/${forecast[i].date.day}`;
-    //   day.title = zip;
-    //   graph.push(day);
-    // }
-    // debugger;
+
     forecast.forEach(z=>charts[zip].dataProvider.push({high:z.high.fahrenheit, low:z.low.fahrenheit, title:`${z.date.month}/${z.date.day}`}));
 
     charts[zip].validateData();
